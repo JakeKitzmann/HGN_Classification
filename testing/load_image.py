3fd3b477b978ca850e5eb79a916b05c4a9d5f3dd
JakeKitzmann <97077033+JakeKitzmann@users.noreply.github.com>
2024-03-10 16:30:31 -0500

    initial commit

---
+++ testing/load_image.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+# Load the image
+image = cv.imread('/Users/jacob_kitz/Desktop/hgn_processing/HGN_Classification/testing/cat.jpeg')
+
+# Check if image loading was successful
+if image is None:
+    print('Could not open or find the image')
+else:
+    # Display the image
+    cv.imshow('image', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
