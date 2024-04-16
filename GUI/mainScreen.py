06cda94b939293e70b673909e2a8a99e2f12bbda
EvinB <evinbayer13@gmail.com>
2024-04-16 18:56:26 -0500

    more main screen updates

--- GUI/mainScreen.py
+++ GUI/mainScreen.py
@@ -2,8 +2,6 @@ import customtkinter as CTk
 import tkinter as tk
 from record import recordVideo
 import threading 
-from customtkinter import CTkFont
-
 from PIL import ImageFont
 
 # Load the font file
@@ -16,7 +14,8 @@ CTk.set_appearance_mode("System")  # Can be "System", "Dark", or "Light"
 CTk.set_default_color_theme("blue")  # Sets the default color theme
 
  
-screenWidth = 0
+screenWidth = 1920
+screenHeight = 1080
 move = None
 
 ################################################################################################################################################
@@ -37,15 +36,19 @@ class MainApplication(CTk.CTk):
         self.testing_frame.pack_forget()
 
         # Create a frame to act as the box
-        intro_box = CTk.CTkFrame(self.main_frame, width = 700, height = 400, corner_radius = 10, bg_color = "blue")
+        intro_box = CTk.CTkFrame(self.main_frame, corner_radius=10, fg_color="#3b8ed0")
         intro_box.place(anchor="center", relx=0.25, rely=0.5)
+        intro_box.place_configure(width=screenWidth/2, height=screenHeight)
 
+        #Title 
+        title_text = CTk.CTkLabel(intro_box, text="The HNG Test", font = (sofiaPro, 48, "bold"), text_color = "white")
+        title_text.pack()
         #intro
         directionsText = "Directions: Keep head 4-6 inches away from webcam and keep "
         HNGintro = "The Horizontal Gaze Nystagmus Test (HGN) is a screening method commonly utilized in various contexts to assess eye movement patterns. It involves tracking the movement of an object with the eyes, observing for any irregularities in the smoothness and continuity of gaze. In a typical HGN test, the subject is instructed to follow a moving target horizontally across their field of vision while maintaining a stable head position. The test aims to identify any deviations from normal eye movement patterns, which may indicate underlying physiological or neurological conditions. By analyzing the quality and consistency of gaze tracking, HGN testing can provide valuable insights into visual function and ocular health."
-        directions_text = CTk.CTkLabel(intro_box, text=HNGintro, wraplength= 650, font = (sofiaPro, 24))
+        directions_text = CTk.CTkLabel(intro_box, text=HNGintro, wraplength= 900, font = (sofiaPro, 24), text_color = "white", justify = "left")
         #directions_text.place(anchor="center", relx=0.25, rely=0.5)
-        directions_text.pack()
+        directions_text.pack(fill = "both", expand = True)
        
 
 
